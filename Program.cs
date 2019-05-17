b1ccd1cfe2a8d7a2c70ef6ecd9e27bf724b1ca02
Manuela174 <45518560+Manuela174@users.noreply.github.com>
2019-05-17 14:23:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PisanjeUDatoteki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Process[] proc = Process.GetProcesses();
+            string lokacijaspremanja = @"D:\";
+            using(StreamWriter sw = new StreamWriter(lokacijaspremanja+"Tekst.txt"))
+            {
+               
+                foreach (Process p in proc)
+                {
+                    sw.WriteLine("{0,-20} {1,10} {2,20}", p.ProcessName, p.Id, p.WorkingSet64);
+                }
+            }
+
+            using (StreamReader sr=new StreamReader(lokacijaspremanja + "Tekst.txt"))
+            {
+                string line;
+                int brojac = 0;
+                while((line=sr.ReadLine()) !=null)
+                {
+                    brojac++;
+                    Console.WriteLine(line);
+                }
+                Console.WriteLine("====================================================================");
+                Console.WriteLine(brojac);
+            }
+        }
+    }
+}
